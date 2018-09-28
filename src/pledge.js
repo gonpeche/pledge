b980cc97aaf517a9c15d6c19fa76dfb083979178
gonpeche <espeche.g@gmail.com>
2018-09-28 13:05:01 -0300

    update

--- src/pledge.js
+++ src/pledge.js
@@ -3,9 +3,29 @@
 Promises Workshop: construye la libreria de ES6 promises, pledge.js
 ----------------------------------------------------------------*/
 // TU CÓDIGO AQUÍ:
+var $Promise = function (fn) {
+    if (typeof fn !== 'function') throw TypeError ('/executor.+function/i')
+    this._state = 'pending';
 
+    this.executor = fn;
 
+    this.executor(this._internalResolve.bind(this), this._internalReject.bind(this))
 
+}
+
+$Promise.prototype._internalResolve = function(someData) {
+    if (this._state === 'pending') {
+        this._state = 'fulfilled';
+        this._value = someData;
+    }
+};
+
+$Promise.prototype._internalReject = function (value) {
+    if (this._state === 'pending') {
+        this._state = 'rejected';
+        this._value = value;
+    }
+};
 
 
 
